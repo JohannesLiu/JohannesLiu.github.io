d4eb5a308c685ac7b73c3ad933ea17210a05396d
johannesliu <johannesliu@live.com>
2020-01-27 13:47:02 +0800

    Site updated: 2020-01-27 13:47:01

--- js/schemes/muse.js
+++ js/schemes/muse.js
@@ -1,3 +1,3 @@
-// build time:Sun Jan 19 2020 23:38:02 GMT+0800 (China Standard Time)
+// build time:Mon Jan 27 2020 13:46:55 GMT+0800 (China Standard Time)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 
